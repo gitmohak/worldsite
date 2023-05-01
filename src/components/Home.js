d1c45fc4af9defeee70cedab6bde77877e355149
gitmohak <aroramohak25@gmail.com>
2023-05-01 23:16:30 +0530

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+        <div className='animation1'></div>
+        <div className='animation2'></div>
+        <div className='animation3'></div>
+            <div className='container'>
+                <img className='home-img' src="./images/building.png" alt="Beautiful Landmark with Doem top" />
+                <img className='home-bg' src="./images/home-background.png" alt="Background Style for Home" />
+
+                <div className='d-flex flex-column justify-content-center align-items-center home-left-section'>
+                    <h1 className='home-heading mt-0 mb-0'>Visit the Entire World</h1>
+                    <p className='home-info mb-5 mt-4'>World Site provides extremely interesting and useful information about the world. You can go to various fantastic places in the world just by visiting here.</p>
+                    <button className='btn btn-primary'>Watch Trailer</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
