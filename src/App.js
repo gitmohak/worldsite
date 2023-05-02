edf16c69599fcffcebdeba77af046fbab0f61177
gitmohak <aroramohak25@gmail.com>
2023-05-02 16:46:25 +0530

    About Page done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Highlights from './components/Highlights';
 import Articles from './components/Articles';
+import About from './components/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Home/>
     <Highlights/>
     <Articles/>
+    <About/>
     </>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className='container d-flex flex-wrap justify-content-between about'>
+            <h1 className='w-100'>About</h1>
+            <div className="card">
+            <a href='/'>
+                <img src="https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body text-center">
+                        <h5 className="card-title fs-3">World Site is Incredible</h5>
+                        <p className="card-text">You will find incredible content about the world here. These places will blow your mind with awesomeness.</p>
+                        <button className='btn btn-primary'>Go Somewhere</button>
+                    </div>
+            </a>
+            </div>
+
+            <div className="card">
+                <a href='/'>
+                <img src="https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body text-center">
+                        <h5 className="card-title fs-3">World Site is Incredible</h5>
+                        <p className="card-text">You will find incredible content about the world here. These places will blow your mind with awesomeness.</p>
+                        <button className='btn btn-primary'>Go Somewhere</button>
+                    </div>
+                    </a>
+            </div>
+
+            <div className="card">
+                <a href='/'>
+                <img src="https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg" className="card-img-top" alt="..."/>
+                    <div className="card-body text-center">
+                        <h5 className="card-title fs-3">World Site is Incredible</h5>
+                        <p className="card-text">You will find incredible content about the world here. These places will blow your mind with awesomeness.</p>
+                        <button className='btn btn-primary'>Go Somewhere</button>
+                    </div>
+                    </a>
+            </div>
+        </div>
+    )
+}
+
+export default About
