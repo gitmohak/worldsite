16ca0a93dbc90706d16a687c26a8a2b14501720e
gitmohak <aroramohak25@gmail.com>
2023-05-04 14:39:50 +0530

    before adjusting cards

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,29 @@
-import React from 'react'
+import React from 'react';
+import data from "../aboutData.json";
+import parse from 'html-react-parser';
 
 function About() {
-    return (
-        <section className='container d-flex flex-wrap justify-content-between about'>
-            <h1 className='w-100'>About us</h1>
+    let arr = [];
+    for(let i in data){
+        arr.push(
+
             <div className="card">
-            <a href='/'>
-                <img src="https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg" className="card-img-top" alt="..."/>
+            <a href={`/about/${i}`}>
+                <img src={data[i].image1} className="card-img-top" alt="Great region"/>
                     <div className="card-body text-center">
-                        <h5 className="card-title fs-3">World Site is Incredible</h5>
-                        <p className="card-text">You will find incredible content about the world here. These places will blow your mind with awesomeness.</p>
-                        <button className='btn btn-primary'>Go Somewhere</button>
+                        <h5 className="card-title fs-3">{parse(data[i].title)}</h5>
+                        <p className="card-text">{parse(data[i].description)}</p>
+                        <button className='btn btn-primary'>Read More</button>
                     </div>
             </a>
             </div>
+        ); 
+    }
 
-            <div className="card">
-                <a href='/'>
-                <img src="https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg" className="card-img-top" alt="..."/>
-                    <div className="card-body text-center">
-                        <h5 className="card-title fs-3">World Site is Incredible</h5>
-                        <p className="card-text">You will find incredible content about the world here. These places will blow your mind with awesomeness.</p>
-                        <button className='btn btn-primary'>Go Somewhere</button>
-                    </div>
-                    </a>
-            </div>
-
-            <div className="card">
-                <a href='/'>
-                <img src="https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg" className="card-img-top" alt="..."/>
-                    <div className="card-body text-center">
-                        <h5 className="card-title fs-3">World Site is Incredible</h5>
-                        <p className="card-text">You will find incredible content about the world here. These places will blow your mind with awesomeness.</p>
-                        <button className='btn btn-primary'>Go Somewhere</button>
-                    </div>
-                    </a>
-            </div>
+    return (
+        <section className='container d-flex flex-wrap justify-content-between about'>
+            <h1 className='w-100'>About us</h1>
+            {arr}
         </section>
     )
 }
