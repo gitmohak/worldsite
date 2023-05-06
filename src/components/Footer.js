1dcacfa9bea58c02f2f13c337da5ac0b664366ce
gitmohak <aroramohak25@gmail.com>
2023-05-06 17:13:53 +0530

    doing dark mode

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,14 @@ function Footer() {
     return (
         <footer className='container d-flex justify-content-between align-items-center foot'>
             <div>© 2023 World Site, Inc. All rights reserved.</div>
-            <img src="./images/logo.png" alt="" />
+            <img src="./images/logo.png" alt="beautiful website" />
             <div className='fs-3'>
-                <i className="bi bi-linkedin"></i>
-                <i className="bi bi-twitter mx-4"></i>
-                <i className="bi bi-facebook"></i>
+                <a href="https://www.twitter.com/itsmohak/" target="_blank" rel="noreferrer"><i className="bi bi-twitter"></i></a>
+
+                <a href="https://www.linkedin.com/in/mohakarora/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin mx-4"></i></a>
+
+
+                <a href="https://www.facebook.com/aroramohak28/" target="_blank" rel="noreferrer"><i className="bi bi-facebook"></i></a>
             </div>
         </footer>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,8 @@ import doScrollSpy from "../methods/scrollSpy";
 
 function Navbar() {
     const [markSection, setmarkSection] = useState("myHome");
+    const [mode, setMode] = useState("light");
+
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -11,15 +13,33 @@ function Navbar() {
         doScrollSpy(location, setmarkSection);
     }
 
+    const darkModeFunc = () =>{
+        mode === "light" ? setMode("dark") : setMode("light");
+    }
+
+    const doDecoration = (ms) => {
+        if(markSection === ms){
+            if(mode === "light"){
+                return "text-decoration-underline text-success";
+            }
+            else if(mode === "dark"){
+                return "text-decoration-underline darkModeGreen";
+            }
+        }
+        else{
+            return "";
+        }
+    }
+
     return (
-        <nav className="navbar navbar-expand-lg myNav">
+        <nav className={`navbar navbar-expand-lg myNav ${mode === "dark" ? "bg-black" : ""}`}>
             <div className="container-fluid">
 
                 <a onClick={() => {
                     navigate("/");
                     setmarkSection("myHome");
                 }}
-                    className="navbar-brand text-success" href="#myHome">World Site</a>
+                    className={`navbar-brand ${mode === "light" ? "text-success" : "darkModeGreen"}`} href="#myHome">World Site</a>
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -32,7 +52,7 @@ function Navbar() {
                                 navigate("/");
                                 setmarkSection("myHome");
                             }}
-                                className={`nav-link fs-5 fw-bold text-primary ${markSection === "myHome" ? "text-decoration-underline text-success" : ""}`} aria-current="page" href="#myHome">Home</a>
+                                className={`nav-link fs-5 fw-bold lightModeBlue ${mode === "light" ? "text-primary" : "darkModeBlue"} ${doDecoration("myHome")}`} aria-current="page" href="#myHome">Home</a>
 
                         </li>
 
@@ -41,7 +61,7 @@ function Navbar() {
                             <a onClick={() => {
                                 navigate("/");
                             }}
-                                className={`nav-link fs-5 fw-bold ${markSection === "myCarousel" ? "text-decoration-underline text-success" : ""}`} aria-current="page" href="#myCarousel">Featured</a>
+                                className={`nav-link fs-5 fw-bold ${doDecoration("myCarousel")}`} aria-current="page" href="#myCarousel">Featured</a>
 
                         </li>
 
@@ -50,7 +70,7 @@ function Navbar() {
                             <a onClick={() => {
                                 navigate("/");
                             }}
-                                className={`nav-link fs-5 fw-bold ${markSection === "myHighlights" ? "text-decoration-underline text-success" : ""}`} aria-current="page" href="#myHighlights">Highlights</a>
+                                className={`nav-link fs-5 fw-bold ${doDecoration("myHighlights")}`} aria-current="page" href="#myHighlights">Highlights</a>
 
                         </li>
 
@@ -59,7 +79,7 @@ function Navbar() {
                             <a onClick={() => {
                                 navigate("/");
                             }}
-                                className={`nav-link fs-5 fw-bold ${markSection === "myAbout" ? "text-decoration-underline text-success" : ""}`} aria-current="page" href="#myAbout">About us</a>
+                                className={`nav-link fs-5 fw-bold ${doDecoration("myAbout")}`} aria-current="page" href="#myAbout">About us</a>
 
                         </li>
 
@@ -69,7 +89,7 @@ function Navbar() {
                                 window.scrollTo(0, 0);
                                 setmarkSection("myAllArticles");
                             }}
-                                className={`nav-link fs-5 fw-bold text-primary ${markSection === "myAllArticles" ? "text-decoration-underline text-success" : ""}`} aria-current="page" to="/all">All Articles</Link>
+                                className={`nav-link fs-5 fw-bold text-primary ${doDecoration("myAllArticles")}`} aria-current="page" to="/all">All Articles</Link>
                         </li>
 
                         <li className="nav-item">
@@ -77,8 +97,13 @@ function Navbar() {
                             <a onClick={() => {
                                 navigate("/");
                             }}
-                                className={`nav-link fs-5 fw-bold ${markSection === "myContact" ? "text-decoration-underline text-success" : ""}`} aria-current="page" href="#myContact">Contact us</a>
+                                className={`nav-link fs-5 fw-bold ${doDecoration("myContact")}`} aria-current="page" href="#myContact">Contact us</a>
 
+                        </li>
+
+                        <li className="nav-item form-check form-switch darkSwitchLi">
+                            <label className={`nav-link fs-5 form-check-label`} aria-current="page" for="darkSwitch">Dark Mode</label>
+                            <input onClick={darkModeFunc} className="form-check-input d-inline" type="checkbox" role="switch" id="darkSwitch" />
                         </li>
                     </ul>
                     {/* <form className="d-flex" role="search">
